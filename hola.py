9c0f1d1ed3c8933734c4c34e2b083de6add99d96
AgentBlowfish <a01705196@tec.mx>
2024-04-17 19:55:10 -0600

    Cambié titanic

--- hola.py
+++ hola.py
@@ -8,11 +8,11 @@ st.title("Titanic App")
 st.header("Titanic graphs")
 st.write("Gráficas del dataset Titanic")
 
-titanic_link = "Titanic.csv"
+titanic_link = "titanic.csv"
 titanic_data = pd.read_csv(titanic_link)
 st.dataframe(titanic_data)
 
-fig, ax = plt.subplots
+fig, ax = plt.subplots()
 ax.hist(titanic_data["Fare"])
 st.header("Histograma del Titanic")
 st.pyplot(fig)
