19d66914bab3c5f5cd6672b0399b902654ecded6
AgentBlowfish <a01705196@tec.mx>
2024-04-17 18:18:54 -0600

    hice mi primera app

---
+++ hola.py
@@ -0,0 +1,7 @@
+# importamos la biblioteca streamlit
+import streamlit as st 
+
+# creamos el titulo de la App
+st.title("Mi primera App")
+st.header("Demo App")
+st.write("Demo de sitio con graficas")
